234a4f4371b2136b1a2799de4ee86274b25c9095
Siddhant <94226898+S1D007@users.noreply.github.com>
2023-05-22 12:07:13 +0530

    Add files via upload

--- src/utils/test-login-or-signup.js
+++ src/utils/test-login-or-signup.js
@@ -4,16 +4,16 @@ const dir = path.join(__dirname, 'test-conditions.json')
 module.exports.isTest = async (req, res) => {
     const data = await fs.readFile(dir, 'utf8');
     res.send({
-        response:JSON.parse(data).isTest
+        response: JSON.parse(data).isTest
     })
 }
 module.exports.setTest = async (req, res) => {
     const data = await fs.readFile(dir, 'utf8');
     const _data = JSON.parse(data);
-    _data.isTest = Boolean(req.query.isTest);
+    _data.isTest = req.body.isTest;
     const __data = JSON.stringify(_data, null, 2);
     await fs.writeFile(dir, __data);
     res.send({
-        response:_data.isTest
+        response: true
     })
 }
