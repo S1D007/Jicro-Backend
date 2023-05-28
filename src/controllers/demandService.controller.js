bb6e3bbb327ba8e992aa296c11d0a714fb5d89b5
Siddhant <94226898+S1D007@users.noreply.github.com>
2023-05-28 08:48:49 +0530

    Update demandService.controller.js

--- src/controllers/demandService.controller.js
+++ src/controllers/demandService.controller.js
@@ -99,10 +99,10 @@ const getDemanedService = async (req, res) => {
 }
 const getDemanedServiceForAll = async (req, res) => {
   const { profession } = req.body;
-  try {
+   try {
     const doc = await DemandService.find({ "type.category": profession }).populate("user");
 
-    const filteredDoc = doc.filter(obj => !obj.hasOwnProperty("serviceProvider"));
+    const filteredDoc = doc.filter(obj => !obj.hasOwnProperty("serviceProvider") || !obj.serviceProvider);
 
     if (filteredDoc.length > 0) {
       res.send({
